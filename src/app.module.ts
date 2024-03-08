132a9432bfe24aad28d2320131768018b2f9c412
AkereleAli <donleeu04@gmail.com>
2024-03-08 10:19:34 +0100

    added console to main file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,22 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { UserModule } from './user/user.module';
-import { User } from './user/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'mysql', // Replace with your database type
       host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: null,
-    database: 'nest-menu',
-      entities: [ User ],
-      synchronize: true,
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nest-menu',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Set to false in production
     }),
-    UserModule
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({whitelist: true, forbidNonWhitelisted:true}));
-  app.enableCors()
-  app.setGlobalPrefix('api/v1')
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
   await app.listen(3123);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
